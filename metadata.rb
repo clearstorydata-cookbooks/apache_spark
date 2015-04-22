8feba68afa9db5cfedc5fd9ce505a2d126f27705
Mikhail Bautin <mbautin@gmail.com>
2015-04-22 12:41:51 -0700

    Update version

--- metadata.rb
+++ metadata.rb
@@ -17,7 +17,7 @@ maintainer 'ClearStory Data, Inc.'
 maintainer_email 'mbautin@clearstorydata.com'
 license 'Apache License 2.0'
 description 'A cookbook to install and configure Apache Spark'
-version '1.0.5'
+version '1.1.0'
 source_url 'https://github.com/clearstorydata-cookbooks/apache_spark'
 issues_url 'https://github.com/clearstorydata-cookbooks/apache_spark/issues'
 
