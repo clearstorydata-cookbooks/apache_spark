08289da224b789ce7b12cab9bf16eb2d9b8f5581
Bill Warner <bwarner@demark.com>
2015-10-07 15:32:00 -0700

    make spark process matching regex a configureable node attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -91,3 +91,8 @@ default['apache_spark']['conf']['spark.deploy.spreadOut'] = true
 default['apache_spark']['conf']['spark.executor.extraLibraryPath'] = '/usr/lib/hadoop/lib/native'
 
 default['apache_spark']['standalone']['local_dirs'] = ['/var/local/spark']
+
+default['apache_spark']['standalone_master']['monit']['process_matcher'] =
+  '^java.* (org\\.apache\\.)?spark\\.deploy\\.master\\.Master '
+default['apache_spark']['standalone_worker']['monit']['process_matcher'] =
+  'java.* org\.apache\.spark\.deploy\.worker\.Worker '
