2793a0785efe43305fa8ef67e6267225266f4fbb
Grant Ridder <shortdudey123@gmail.com>
2016-08-01 22:07:57 -0700

    source_url and issues_url should be guarded with respond_to?

--- metadata.rb
+++ metadata.rb
@@ -18,8 +18,9 @@ maintainer_email 'mbautin@clearstorydata.com'
 license 'Apache License 2.0'
 description 'A cookbook to install and configure Apache Spark'
 version '1.2.14'
-source_url 'https://github.com/clearstorydata-cookbooks/apache_spark'
-issues_url 'https://github.com/clearstorydata-cookbooks/apache_spark/issues'
+
+source_url 'https://github.com/clearstorydata-cookbooks/apache_spark' if respond_to?(:source_url)
+issues_url 'https://github.com/clearstorydata-cookbooks/apache_spark/issues' if respond_to?(:issues_url)
 
 %w(
   centos
